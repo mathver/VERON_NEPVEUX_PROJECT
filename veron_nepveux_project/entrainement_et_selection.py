a96709a521cd2f28ccafa580fa1e2e1f2e3e3652
Mathieu <mathieuveron@outlook.fr>
2023-01-20 10:44:03 +0100

    fgfdg dfg

--- veron_nepveux_project/entrainement_et_selection.py
+++ veron_nepveux_project/entrainement_et_selection.py
@@ -24,7 +24,7 @@ from veron_nepveux_project.scraping import Voiture
 import pandas as pd
 from serde.json import to_json
 from typing import Any, Union
-import pathlib
+import os
 
 STOCKAGE = "./stockage/"
 
@@ -286,7 +286,8 @@ def charge_meilleur_estimateur() -> Any:
 
     Retourne le modèle extrait du fichier pkl.
     """
-    dir = str(pathlib.Path().absolute())
+
+    dir = os.path.dirname(os.path.abspath(__file__))
     path = dir + "\\meilleur_estimateur.pkl"
     with open(path, "rb") as file:
         est = load(file=file)
