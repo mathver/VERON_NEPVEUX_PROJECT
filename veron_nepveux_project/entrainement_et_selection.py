3fa6add84b267b9f9c3631a9420a4647bcd2a198
Mathieu <mathieuveron@outlook.fr>
2023-01-20 10:52:06 +0100

    sdfsdf

--- veron_nepveux_project/entrainement_et_selection.py
+++ veron_nepveux_project/entrainement_et_selection.py
@@ -287,9 +287,7 @@ def charge_meilleur_estimateur() -> Any:
     Retourne le modèle extrait du fichier pkl.
     """
 
-    dir = os.path.dirname(os.path.abspath("entrainement_et_selection.py"))
-    path = dir + "\\meilleur_estimateur.pkl"
-    with open(path, "rb") as file:
+    with open("meilleur_estimateur.pkl", "rb") as file:
         est = load(file=file)
     return est
 
